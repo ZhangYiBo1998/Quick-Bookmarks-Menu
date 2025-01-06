cb8f2f77a87cf332f76d1c737668176f9e2a1e88
ZhangYiBo1998 <27582180@qq.com>
2025-01-06 22:07:21 +0800

    将manifest版本重置回v2，并对v3代码做兼容。 修改拓展icon图标

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -1,7 +1,7 @@
 // 引入所需的库和组件
-import { h, render } from 'preact';
-import { useState } from 'preact/hooks';
-import ContextWrapper, { Configuration, Message, Page } from './components/ContextWrapper';
+import {h, render} from 'preact';
+import {useState} from 'preact/hooks';
+import ContextWrapper, {Configuration, Message, Page} from './components/ContextWrapper';
 import PopupHeader from './components/PopupHeader';
 import PopupContainer from './components/PopupContainer';
 import PopupFooter from './components/PopupFooter';
@@ -13,18 +13,21 @@ interface PopupProps {
     config: Configuration
 }
 
+// 兼容旧版chrome.action API
+const setIcon = chrome.action ? chrome.action.setIcon : chrome.browserAction.setIcon;
+
 // 定义弹出窗口组件
 function Popup(props: PopupProps) {
     // 使用useState定义多个状态变量
     const [config, setConfig] = useState(props.config);
-    const [page, setPage] = useState<Page>({ type: 'folder', key: config.startup[0] });
+    const [page, setPage] = useState<Page>({type: 'folder', key: config.startup[0]});
     const [msgs, setMsgs] = useState<Message[]>([]);
     const [hidden, setHidden] = useState(config.hidden);
     const [removeBookmarksId, setRemoveBookmarksId] = useState('');
 
     // 定义导航函数，用于更新页面状态
     const navigate = (type: Page["type"], key: Page["key"]) => {
-        setPage({ type, key });
+        setPage({type, key});
     };
 
     // 定义消息通知函数，用于向页面添加消息
@@ -46,13 +49,13 @@ function Popup(props: PopupProps) {
         } else {
             newHidden = hidden.concat([key]);
         }
-        chrome.storage.local.set({ hidden: newHidden }, () => {
+        chrome.storage.local.set({hidden: newHidden}, () => {
             setHidden(newHidden);
         });
     };
 
     // 更新收藏夹列表
-    const removeBookmarksCallback = (id:string) => {
+    const removeBookmarksCallback = (id: string) => {
         console.log('更新收藏夹列表')
         setRemoveBookmarksId(id);
     }
@@ -61,10 +64,10 @@ function Popup(props: PopupProps) {
     applyTheme(config.theme);
     return (
         <ContextWrapper nav={navigate} config={[config, setConfig]} notify={notify} hide={setItemHide}>
-            <PopupHeader page={page} msgs={msgs} clearMsg={clearMsg} horiz={config.scroll === 'x'} />
-            <PopupContainer page={page} hidden={hidden} removeBookmarksId={removeBookmarksId} />
-            <PopupFooter page={page} hidden={hidden} />
-            <ContextMenu removeBookmarksCallback={removeBookmarksCallback} />
+            <PopupHeader page={page} msgs={msgs} clearMsg={clearMsg} horiz={config.scroll === 'x'}/>
+            <PopupContainer page={page} hidden={hidden} removeBookmarksId={removeBookmarksId}/>
+            <PopupFooter page={page} hidden={hidden}/>
+            <ContextMenu removeBookmarksCallback={removeBookmarksCallback}/>
         </ContextWrapper>
     );
 }
@@ -72,7 +75,7 @@ function Popup(props: PopupProps) {
 // 加载配置并渲染弹出窗口
 chrome.storage.local.get(['openIn', 'openInMiddle', 'doNotClose', 'hoverEnter', 'startup', 'root', 'theme', 'scroll', 'hidden', 'showHidden'], (result: Partial<Configuration>) => {
     adjustHeight((result as Configuration).startup[1]);
-    render(<Popup config={result as Configuration} />, document.body);
+    render(<Popup config={result as Configuration}/>, document.body);
 });
 
 // 调整弹出窗口的高度
@@ -90,7 +93,7 @@ function applyTheme(theme: 'auto' | 'light' | 'dark') {
     const applyDarkTheme = () => {
         rootElm.classList.add('theme-dark');
         rootElm.classList.remove('theme-light');
-        chrome.action.setIcon({
+        setIcon({
             path: {
                 "16": "/icons/qbm16-dark.png",
                 "32": "/icons/qbm32-dark.png"
@@ -102,7 +105,7 @@ function applyTheme(theme: 'auto' | 'light' | 'dark') {
     const applyLightTheme = () => {
         rootElm.classList.add('theme-light');
         rootElm.classList.remove('theme-dark');
-        chrome.action.setIcon({
+        setIcon({
             path: {
                 "16": "/icons/qbm16.png",
                 "32": "/icons/qbm32.png"
